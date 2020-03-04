fbe4ddfb07136d5d3f1df15ab580d7cd1e44b619
KingLee <376700233@qq.com>
2020-03-04 18:37:06 +0800

    group test

--- learning001/hello_test.go
+++ learning001/hello_test.go
@@ -3,11 +3,30 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Hello")
-	want := "Hello,wor1d"
-
-	if got != want {
-		t.Errorf("got '%q' want '%q'",got,want)
+	assertCorrectMessage := func(t *testing.T,got,want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q",got,want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello,Chris"
+        assertCorrectMessage(t,got ,want)
+		//if got != want {
+		//	t.Errorf("got '%q' want '%q'", got, want)
+		//}
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello,World"
+
+		//if got != want {
+		//	t.Errorf("got '%q' want '%q'", got, want)
+		//}
+		assertCorrectMessage(t,got ,want)
+	})
 
 }
