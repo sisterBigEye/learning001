c09cc6b9fde924606b561bbfa19c59934ad4a81c
KingLee <376700233@qq.com>
2020-03-06 15:51:59 +0800

    add pointer to make it works

--- walletgo/wallet.go
+++ walletgo/wallet.go
@@ -6,11 +6,11 @@ type Wallet struct {
   balance int
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
 	return w.balance
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount int) {
 	fmt.Println("address of balance in Deposit is",&w.balance)
 	w.balance +=  amount
 }
