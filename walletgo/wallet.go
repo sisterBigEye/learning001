593fa45164a35d1f71a32c81f5c72dd313cc43f2
KingLee <376700233@qq.com>
2020-03-06 15:51:19 +0800

    wallet operator fail

---
+++ walletgo/wallet.go
@@ -0,0 +1,16 @@
+package walletgo
+
+import "fmt"
+
+type Wallet struct {
+  balance int
+}
+
+func (w Wallet) Balance() int {
+	return w.balance
+}
+
+func (w Wallet) Deposit(amount int) {
+	fmt.Println("address of balance in Deposit is",&w.balance)
+	w.balance +=  amount
+}
