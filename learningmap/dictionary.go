0c70a13e760dad9676a85cd2ed58dd0e458bf3db
KingLee <376700233@qq.com>
2020-03-09 15:51:38 +0800

    maps go

---
+++ learningmap/dictionary.go
@@ -0,0 +1,64 @@
+package main
+
+const (
+	ErrNotFound = DictionaryErr("could not find the world you were looking for")
+    ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("CANNOT UPDATE WORD because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string{
+	return string(e)
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string,error){
+	definition,ok := d[word]
+	if !ok {
+		return "",ErrNotFound
+	}
+	return definition,nil
+}
+
+func (d Dictionary) Add(word,definition string) error{
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word,definition string) error {
+	_,err := d.Search(word)
+
+	switch err{
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string){
+   delete(d,word)
+}
+//func Search(dictionary map[string]string,world string) string{
+//	return dictionary[world]
+//}
+
+
+////创建空的map
+//dic1 = map[string]string{}
+////or
+//dic = make(map[string]string)
